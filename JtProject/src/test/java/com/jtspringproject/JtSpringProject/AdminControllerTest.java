16ce1314c9a22dcb2884414e4230d7fb8747f120
Julie Manirath <juliemanirath@gmail.com>
2023-08-11 20:36:29 -0400

    [Product] some more

--- JtProject/src/test/java/com/jtspringproject/JtSpringProject/AdminControllerTest.java
+++ JtProject/src/test/java/com/jtspringproject/JtSpringProject/AdminControllerTest.java
@@ -7,11 +7,16 @@ import com.jtspringproject.JtSpringProject.models.User;
 import com.jtspringproject.JtSpringProject.services.userService;
 import com.jtspringproject.JtSpringProject.models.Product;
 import com.jtspringproject.JtSpringProject.services.productService;
+
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
+
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
@@ -23,6 +28,7 @@ import java.util.List;
 import static org.mockito.Mockito.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
+@AutoConfigureMockMvc
 @ExtendWith(MockitoExtension.class)
 public class AdminControllerTest {
 
@@ -30,10 +36,8 @@ public class AdminControllerTest {
 
     @Mock
     private categoryService categoryService;
-
     @Mock
     private userService userService;
-
     @Mock
     private productService productService;
 
@@ -147,8 +151,6 @@ public class AdminControllerTest {
                         .andExpect(redirectedUrl("/admin/category"));
     }
     /**********************************************| Products |***********************************************/
-
-    // getproduct
     @Test
     public void TestGetProduct() throws Exception {
         this.adminController.setAdminlogcheck(1);
@@ -174,13 +176,77 @@ public class AdminControllerTest {
                 .andExpect(model().attributeExists("products"));
     }
 
-    // addproduct - Get
-    // addproduct - Post
-    // updateproductinfo - Get
-    // updateproduct - Post
-    // removeProduct
-    // searchProduct
+    @Test
+    public void TestAddProductGetForm() throws Exception{
+        this.adminController.setAdminlogcheck(1);
+        this.mockMvc = MockMvcBuilders.standaloneSetup(adminController).build();
 
+        mockMvc.perform(MockMvcRequestBuilders.get("/admin/products/add"))
+                .andExpect(status().isOk())
+                .andExpect(view().name("productsAdd"))
+                .andExpect(model().size(2))
+                .andExpect(model().attributeExists("products"))
+                .andExpect(model().attributeExists("categories"));
+    }
+
+    @Test
+    public void TestAddProduct() throws Exception{
+        this.adminController.setAdminlogcheck(1);
+        this.mockMvc = MockMvcBuilders.standaloneSetup(adminController).build();
+
+        Category cat1 = new Category(1,"Fruits");
+        Category cat2 = new Category(2,"Vegetables");
+        Category cat3 = new Category(3,"Meat");
+        Category cat4 = new Category(4,"Fish");
+        Category cat5 = new Category(5,"Dairy");
+        List<Category> categoryList = new ArrayList<>(Arrays.asList(cat1,cat2,cat3,cat4,cat5));
+        when(categoryService.getCategories()).thenReturn(categoryList);
+
+        Product product1 = new Product(1,"Peach", "https://upload.wikimedia.org/wikipedia/en/thumb/1/16/Princess_Peach_Stock_Art.png/220px-Princess_Peach_Stock_Art.png", cat1, 1, 1000.00, 60000,"Some description", null, null );
+        Product product2 = new Product(2,"Cherry", "https://static.libertyprim.com/files/familles/cerise-large.jpg?1569271737", cat1, 500, 3.50, 2500,"Some description", null, null );
+        List<Product> productList = new ArrayList<>(Arrays.asList(product1,product2));
+        when(productService.getProducts()).thenReturn(productList);
+
+        Product newProduct = new Product(3,"Pineapple","https://www.aubut.ca/pictures/products/large/10702_whole-fresh-pineapple-gr6-8.png", cat1, 50, 4.99, 3000, "Trlolololo", product2, null );
+        when(productService.addProduct(newProduct)).thenReturn(newProduct);
+
+        mockMvc.perform(MockMvcRequestBuilders.post("/admin/products/add")
+                        .param("name","Pineapple")
+                        .param("categoryid", String.valueOf(1))
+                        .param("price", String.valueOf(4.99))
+                        .param("weight","3000")
+                        .param("quantity", String.valueOf(50))
+                        .param("description","Trlolololo")
+                        .param("productImage","https://www.aubut.ca/pictures/products/large/10702_whole-fresh-pineapple-gr6-8.png")
+                        .param("suggestedProduct", String.valueOf(product2.getId())))
+                        .andExpect(redirectedUrl("/admin/product"));
+
+    }
+    /*
+    @Test
+    public void TestUpdateProductGetForm() throws Exception{
+        this.adminController.setAdminlogcheck(1);
+        this.mockMvc = MockMvcBuilders.standaloneSetup(adminController).build();
+    }
+
+    @Test
+    public void TestUpdateProduct() throws Exception{
+        this.adminController.setAdminlogcheck(1);
+        this.mockMvc = MockMvcBuilders.standaloneSetup(adminController).build();
+    }
+
+    @Test
+    public void TestRemoveProduct() throws Exception{
+        this.adminController.setAdminlogcheck(1);
+        this.mockMvc = MockMvcBuilders.standaloneSetup(adminController).build();
+    }
+
+    @Test
+    public void TestSearchProduct() throws Exception{
+        this.adminController.setAdminlogcheck(1);
+        this.mockMvc = MockMvcBuilders.standaloneSetup(adminController).build();
+    }
+     */
     /**********************************************| Users/Customers |***********************************************/
 
     // getCustomerDetail
@@ -188,5 +254,4 @@ public class AdminControllerTest {
     // removeUser
     // displayUpdateUserInfo - Get
     // updateUserInfo - Post
-
 }
